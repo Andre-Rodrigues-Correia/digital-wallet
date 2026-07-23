36ea740ead7f284fda0c1f5563db7dc9b318eedf
André Correia <andrecorreiati@gmail.com>
2026-07-23 19:13:36 -0300

    chore: create modules, controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { WalletModule } from './wallet/wallet.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, WalletModule, AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
