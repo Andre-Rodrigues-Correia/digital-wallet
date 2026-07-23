7337a1bf5a59c2c7bf3e6460bf8667035e839ea7
André Correia <andrecorreiati@gmail.com>
2026-07-23 19:53:48 -0300

    feat/config mongodb and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from './confg/swagger/swagger.config';
+
+import { ValidationPipe } from '@nestjs/common';
+
+import helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+      }),
+  );
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
